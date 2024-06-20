334428039ff2620ec1b5df16f82515cf931c32ba
JonasBrilhante <jonasbrilhante@yahoo.com.br>
2024-06-20 15:18:41 -0300

    fiz uma alteração

--- ProjetoGitHub/ProjetoGitHub/Form1.Designer.cs
+++ ProjetoGitHub/ProjetoGitHub/Form1.Designer.cs
@@ -30,44 +30,57 @@
         {
             button1 = new Button();
             bt_mensagem = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(465, 237);
-            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Location = new Point(664, 395);
             button1.Name = "button1";
-            button1.Size = new Size(78, 20);
+            button1.Size = new Size(111, 33);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // bt_mensagem
             // 
-            bt_mensagem.Location = new Point(12, 234);
+            bt_mensagem.Location = new Point(17, 390);
+            bt_mensagem.Margin = new Padding(4, 5, 4, 5);
             bt_mensagem.Name = "bt_mensagem";
-            bt_mensagem.Size = new Size(130, 23);
+            bt_mensagem.Size = new Size(186, 38);
             bt_mensagem.TabIndex = 1;
             bt_mensagem.Text = "Exibir Mensagem";
             bt_mensagem.UseVisualStyleBackColor = true;
             bt_mensagem.Click += bt_mensagem_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(328, 166);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 25);
+            label1.TabIndex = 2;
+            label1.Text = "OI EVELYN";
+            label1.Click += label1_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(560, 270);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(bt_mensagem);
             Controls.Add(button1);
-            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private Button bt_mensagem;
+        private Label label1;
     }
 }
--- ProjetoGitHub/ProjetoGitHub/Form1.cs
+++ ProjetoGitHub/ProjetoGitHub/Form1.cs
@@ -11,5 +11,10 @@ namespace ProjetoGitHub
         {
             MessageBox.Show("bem-vindo ao GITHUB!");
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
